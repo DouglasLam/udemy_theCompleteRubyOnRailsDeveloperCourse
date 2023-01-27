5b12f5c9ce813ce6f154e9bb16239729fc4f9ab2
doulam <doulam@cisco.com>
2023-01-26 23:21:53 -0700

    Lecture 222 Task 4: Build User resource - Create User in database - Add gem 'hirb' to pretty up console.  irb> Hirb.enable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Doug1", password: "password")
+User.create(username: "Doug2", password: "password")
+User.create(username: "Doug3", password: "password")
+User.create(username: "Doug4", password: "password")
+User.create(username: "Doug5", password: "password")
