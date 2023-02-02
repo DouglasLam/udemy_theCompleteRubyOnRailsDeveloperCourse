0ea1f26087877e0dfe93489970f11d1b1e809246
doulam <doulam@cisco.com>
2023-02-02 14:58:40 -0700

    Lecture 224 Task 6: Add actual messages from table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_27_061438) do
+ActiveRecord::Schema.define(version: 2023_02_02_211351) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
